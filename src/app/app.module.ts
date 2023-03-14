00bedbd838dfb656aed415b9d41025e4b8054b67
mofirojean <mofirojean@gmail.com>
2023-03-14 14:10:09 +0100

    feat: created essential components of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccessPointComponent } from './components/access-point/access-point.component';
+import { ElementsComponent } from './components/elements/elements.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccessPointComponent,
+    ElementsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
